c1bf944d373df7fe843cdfd9d2ef85afbfed0a82
Roman <akademiyashah@gmail.com>
2022-10-19 15:30:28 +0300

    winform

--- TeamProject/MainWindow.xaml.cs
+++ TeamProject/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace TeamProject
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UserForm userForm = new UserForm();
+            userForm.ShowDialog();
+        }
     }
 }
