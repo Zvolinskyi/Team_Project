6742d5ee495d35d32e3a1fb2c0c72fea2c6ac1e0
Roman <akademiyashah@gmail.com>
2022-10-19 16:59:54 +0300

    Background

--- TeamProject/MainWindow.xaml.cs
+++ TeamProject/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading;
+
 namespace TeamProject
 {
     /// <summary>
@@ -20,15 +22,71 @@ namespace TeamProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+        Rect playerHitBox;
+        Rect groundHitBox;
+        Rect obstacleHitBox;
+
+        bool jumping;
+
+        int force = 20;
+        int speed = 5;
+        Random rnd = new Random();
+
+        bool gameOver;
+
+        double spriteIndex = 0;
+
+        ImageBrush playerSprite = new ImageBrush();
+        ImageBrush backgroundSprite = new ImageBrush();
+        ImageBrush obstacleSprite = new ImageBrush();
+
+        int[] obstaclePosition = { 320, 310, 300, 305, 315 };
+        int score = 0;
         public MainWindow()
         {
             InitializeComponent();
+
+
+            MyCanvas.Focus();
+
+            gameTimer.Tick += GameEngine;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            backgroundSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/background.jpg"));
+
+            background.Fill = backgroundSprite;
+            background.Fill = backgroundSprite;
+
+        }
+
+        private void GameEngine(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             UserForm userForm = new UserForm();
             userForm.ShowDialog();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+        private void StartGame()
+        {
+
+        }
+        private void RunSprite()
+        {
+
         }
     }
 }
